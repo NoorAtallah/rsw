070e3a5fa5aec8cb479f0f8f32256cb815b7993c
DigitalCore360 <nooratallah1234@gmail.com>
2026-02-20 14:57:43 +0300

    new edits

--- my-next-app/lib/sanity.client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'next-sanity'
-
-export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  apiVersion: '2024-01-01',
-  useCdn: false, // We'll set this to true later for production
-})
